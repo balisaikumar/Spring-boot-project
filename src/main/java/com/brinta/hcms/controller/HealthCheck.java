208af42c0d54f24d01d2fde3e4c20e24e12b0e4b
sowmyasiddamurthy <sowmya.siddamurthy@brinta.in>
2025-06-04 11:38:44 +0530

    Initial project setup

--- src/main/java/com/brinta/hcms/controller/HealthCheck.java
+++ src/main/java/com/brinta/hcms/controller/HealthCheck.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/hcms-java/health")
 public class HealthCheck {
 
     @GetMapping("/check")
